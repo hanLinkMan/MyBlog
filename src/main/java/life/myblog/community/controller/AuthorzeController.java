8137fb3967c90135265f83d5c4b1d2e4c3c30196
hanLinkMan <363427029@qq.com>
2019-07-29 15:34:16 +0800

    add cookie

--- src/main/java/life/myblog/community/controller/AuthorzeController.java
+++ src/main/java/life/myblog/community/controller/AuthorzeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 /**
@@ -39,7 +41,8 @@ public class AuthorzeController {
     @GetMapping("/callback")
     public  String callback(@RequestParam(name="code")String code,
                             @RequestParam(name="state")String state,
-                            HttpServletRequest request){
+                            HttpServletRequest request,
+                            HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -50,14 +53,18 @@ public class AuthorzeController {
         GithubUser githubUser = gitProvider.getUser(accessToken);
         if (githubUser != null) {
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
+            //写入cookie
+            response.addCookie(new Cookie("token",token));
             //登陆成功，写cookie和session
-            request.getSession().setAttribute("user",githubUser);
+//            request.getSession().setAttribute("user",githubUser);
+
             return "redirect:index";
         }else {
             //登陆失败，重新登陆
--- src/main/java/life/myblog/community/mapper/UserMapper.java
+++ src/main/java/life/myblog/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package life.myblog.community.mapper;
 import life.myblog.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,4 +17,6 @@ public interface UserMapper {
 
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
